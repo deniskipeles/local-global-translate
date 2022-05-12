9591acd62693b92b20c1ab581506d4ded908bbcd
deniskipeles <narydbest@gmail.com>
2022-05-12 20:05:56 +0300

    initial commit

--- global_to_local_dict_lib/audio-player.jsx
+++ global_to_local_dict_lib/audio-player.jsx
@@ -2,58 +2,6 @@ import React, { useState, useEffect } from "react";
 
 
  
-
-
-/*
-const Player = () => {
-  try {
-      const AudioContext =
-        window.OfflineAudioContext ||
-        window.webkitOfflineAudioContex;
-      const context = new AudioContext(1, 5000, 44100)
-      
-      const oscillator = context.createOscillator()
-      oscillator.type = "triangle"
-      oscillator.frequency.value = 1000
-      
-      const compressor = context.createDynamicsCompressor()
-      compressor.threshold.value = -50
-      compressor.knee.value = 40
-      compressor.ratio.value = 12
-      compressor.reduction.value = 20
-      compressor.attack.value = 0
-      compressor.release.value = 0.2
-      
-      oscillator.connect(compressor)
-      compressor.connect(context.destination);
-      
-      oscillator.start()
-      context.oncomplete = event => {
-        // We have only one channel, so we get it by index
-        const samples = event.renderedBuffer.getChannelData(0)
-      };
-      context.startRendering()
-      
-      function calculateHash(samples) {
-        let hash = 0
-        for (let i = 0; i < samples.length; ++i) {
-          hash += Math.abs(samples[i])
-        }
-        return hash
-      }
-      
-  } catch (err) {
-    console.log("Oops, `window` is not defined")
-  }
-
-  return (
-    <div>
-      <button onClick={()=>{}}>Audio</button>
-    </div>
-  );
-};
-*/
-
 const Player = ({obj}) => {
   return(
     <div id="audiowrap">
